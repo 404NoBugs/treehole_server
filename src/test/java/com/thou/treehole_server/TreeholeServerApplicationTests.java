2561a5667fe19f103636df4bfe2ffc775f547555
404NoBugs <tongxuguang23@stu.xju.edu.cn>
2022-07-26 07:33:28 +0800

    初始化

---
+++ src/test/java/com/thou/treehole_server/TreeholeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thou.treehole_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeholeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
